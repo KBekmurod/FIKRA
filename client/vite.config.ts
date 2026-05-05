16196d55b1d717f77cfd5e7a7db704e464489787
Bekmurod <142839214+KBekmurod@users.noreply.github.com>
2026-05-05 15:34:18 +0000

    loading manifest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico'],
       manifest: {
         name: 'FIKRA - DTM Testlar',
         short_name: 'FIKRA',
@@ -19,14 +19,9 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/icon.svg',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
